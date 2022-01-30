9d45d475f85f45fb8ae5789a0075ff69ac93f6eb
Sarvarbek <sarvarbekodilov070603@gmail.com>
2022-01-31 01:45:01 +0500

    bot

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -2,6 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '5057668685:AAFc4ELEfQFSHYQKA6aeTs2lpEtCrhafdo4';
 const bot = new TelegramBot(token, {polling: true});
 
+bot.on(message, (msg) =>{
+    bot.sendMessage(msg.chat.id, "Salom bizning sayt pressa.uz")
+})
+
 const BOT =  (req,res,next) => {
     console.log('salom')
     bot.sendMessage(887528138, req.postId)
