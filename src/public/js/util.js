1dc4a12f4e1e65c566c7ec7fa153d79d13dbb251
Sarvarbek <sarvarbekodilov070603@gmail.com>
2022-02-05 00:00:37 +0500

    elon berish

--- src/public/js/util.js
+++ src/public/js/util.js
@@ -1,4 +1,4 @@
-const backendApi = 'http://telegram-back-end.herokuapp.com'      ///telegram-back-end.herokuapp.com
+const backendApi = 'https://telegram-back-end.herokuapp.com'      ///telegram-back-end.herokuapp.com
 
 async function request (route, method, body) {
 	let headers = {
